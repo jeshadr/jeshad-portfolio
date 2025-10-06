62f4b91e0b4a5bf06f4028d2b5bc9c9aae6ca2e2
Jeshad <jeshadrahman@gmail.com>
2025-10-06 02:24:19 -0700

    Added Google Search verification and renamed resume

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,10 @@ const font = Figtree({
 
 export const metadata: Metadata = {
   title: "Jeshadify - Portfolio",
-  description: "A Spotify themed portfolio that lets you explore my work, resume, tech stack, and contack details.",
+  description: "A Spotify themed portfolio that lets you explore my work, resume, tech stack, and contact details.",
+  verification: {
+    google: "xwAF9BiJ20IIsZ8AjF1I1fFkTbXBY5122ZF4NW_9b3U",
+  },
 };
 
 export default function RootLayout({
